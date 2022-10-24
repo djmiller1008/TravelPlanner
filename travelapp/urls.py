d63636f4da76da29166373bdb77bd815f959fc99
David <djmiller1008@yahoo.com>
2022-10-24 11:21:51 +0800

    Add discover path to urls

--- travelapp/urls.py
+++ travelapp/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path("", views.index, name='index'),
-    path("login/", views.login_view, name='login'),
-    path("register/", views.register, name='register'),
-    path("logout/", views.logout_view, name='logout')
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("register", views.register, name="register"),
+    path("logout", views.logout_view, name="logout"),
+    path("discover", views.discover, name="discover")
 ]
