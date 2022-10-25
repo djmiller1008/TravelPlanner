75515140846a0d1cf135140f28c81883856ff505
David <djmiller1008@yahoo.com>
2022-10-25 15:31:46 +0800

    Add methods to fetch results from a radius search of a destination, and to get the count of interesting places around that destination

--- travelapp/static/travelapp/api_util.js
+++ travelapp/static/travelapp/api_util.js
@@ -3,8 +3,19 @@ import { apiKey } from "./keys.js";
 
 //Fetches a single destination based on the users search query
 export const searchDestination = async query => {
-    const response = await fetch(`https://api.opentripmap.com/0.1/en/places/geoname?apikey=${apiKey}&name=${query}`)
+    const response = await fetch(`https://api.opentripmap.com/0.1/en/places/geoname?apikey=${apiKey}&name=${query}`);
     const result = await response.json();
     return result;
 }
 
+export const searchDestinationRadius = async (lat, lon, radius, offset, pageLength) => {
+    const response = await fetch(`https://api.opentripmap.com/0.1/en/places/radius?apikey=${apiKey}&limit=${pageLength}&radius=${radius}&lon=${lon}&lat=${lat}&offset=${offset}`);
+    const result = await response.json();
+    return result;
+}
+
+export const getCountOfInterestingPlaces = async(lat, lon, radius, offset, pageLength) => {
+    const response = await fetch(`https://api.opentripmap.com/0.1/en/places/radius?apikey=${apiKey}&limit=${pageLength}&radius=${radius}&lon=${lon}&lat=${lat}&offset=${offset}&format=count`);
+    const result = await response.json();
+    return result.count;
+}
