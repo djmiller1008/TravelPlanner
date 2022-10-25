36fb0eb63f2034d990bea3047671eb6abf0611a3
David <djmiller1008@yahoo.com>
2022-10-25 13:55:40 +0800

    Add url for discover destination, page that will list details about a destination

--- travelapp/urls.py
+++ travelapp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("register", views.register, name="register"),
     path("logout", views.logout_view, name="logout"),
-    path("discover", views.discover, name="discover")
+    path("discover", views.discover, name="discover"),
+    path("discover/<str:name>", views.discover_destination, name="discover_destination")
 ]
