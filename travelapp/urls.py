aa107b8df02a8a331e6624415288e7cc29de8c09
David <djmiller1008@yahoo.com>
2022-10-28 23:04:13 +0800

    Add routes for plan and trips pages

--- travelapp/urls.py
+++ travelapp/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("logout", views.logout_view, name="logout"),
     path("discover", views.discover, name="discover"),
-    path("discover/<str:name>", views.discover_destination, name="discover_destination")
+    path("discover/<str:name>", views.discover_destination, name="discover_destination"),
+    path("plan", views.plan_solo_trip, name="plan"),
+    path("trips", views.trips, name="trips")
 ]
