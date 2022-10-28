c3f90331aa25102f7e84439f1a609a80144b9e02
David <djmiller1008@yahoo.com>
2022-10-28 23:05:37 +0800

    Register models to admin interface

--- travelapp/admin.py
+++ travelapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import User, SoloTrip, SoloDayItinerary, SoloVisitLandmark
+
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(SoloTrip)
+admin.site.register(SoloDayItinerary)
+admin.site.register(SoloVisitLandmark)
