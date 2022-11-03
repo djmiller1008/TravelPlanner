03d374e99ac28c90d1dd688752a8b1d8d935e502
David <djmiller1008@yahoo.com>
2022-11-03 12:42:06 +0800

    Add route to add a solo day itinerary

--- travelapp/urls.py
+++ travelapp/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path("plan", views.plan_solo_trip, name="plan"),
     path("trips", views.trips, name="trips"),
     path("trips/<int:pk>", views.trip_show, name="trip_show"),
-    path("solo_day_itineraries/<int:trip_id>", views.solo_day_itineraries, name="solo_day_itineraries")
+    path("solo_day_itineraries/<int:trip_id>", views.solo_day_itineraries, name="solo_day_itineraries"),
+    path("add_solo_day_itinerary/<int:trip_id>", views.add_solo_day_itinerary, name="add_solo_day_itinerary")
 ]
