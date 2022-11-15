275d3452f1c2c335ac7aca5211086a5e023ad00f
David <djmiller1008@yahoo.com>
2022-11-15 22:41:51 +0800

    Add new javascipt file for planning a trip page, when a user creates their trip the name of the destination is searched in the api to find the lat and lon of the destination so we can add it to the model, if the destination is only a partial match or is not found by the api we alert the user

---
+++ travelapp/static/travelapp/plan.js
@@ -0,0 +1,24 @@
+import * as APIUtil from './api_util.js'
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    let createButton = document.getElementById('create-trip-button');
+    createButton.addEventListener('click', async event => {
+        event.preventDefault();
+        const destination = document.querySelectorAll('input')[1].value;
+        const result = await APIUtil.searchDestination(destination);
+        console.log(result);
+        
+        if (result.partial_match === true) {
+            alert('Invalid Destination');
+            return;
+        } else if (result.lat === undefined) {
+            alert('Invalid Destination');
+            return;
+        } else {
+            document.getElementsByName('lat')[0].value = result.lat;
+            document.getElementsByName('lon')[0].value = result.lon; 
+            document.getElementById('trip-form').submit()
+        }
+    })
+})
