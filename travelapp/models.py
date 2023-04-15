e165aea0af05b8429ad1ada09c4fbf3d6a50f81e
David <djmiller1008@yahoo.com>
2023-04-15 09:46:36 +0800

    Spacing

--- travelapp/models.py
+++ travelapp/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 class User(AbstractUser):
     pass
 
@@ -23,9 +22,6 @@ class SoloTrip(models.Model):
             return False 
         else:
             return True
-
-
-   
 
 class SoloDayItinerary(models.Model):
     day_number = models.IntegerField(blank=False)
